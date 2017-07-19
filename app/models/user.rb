90e8caee36fa95dc8fe7a8356e92987634a4e5ca
Sean <swbrooks87@gmail.com>
2017-07-19 08:30:39 -0400

    Added devise user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
