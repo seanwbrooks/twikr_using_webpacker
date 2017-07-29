5d5f206dafd4e9fc5da6c7d1a704aa22fd7305b1
Sean <swbrooks87@gmail.com>
2017-07-29 10:37:20 -0400

    Added default url for production to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "stock_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  #config database for Heroku
+  cofig.action_mailer.default_url_options = { host: 'https://twikr.herokuapp.com/', port: 3000 }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
