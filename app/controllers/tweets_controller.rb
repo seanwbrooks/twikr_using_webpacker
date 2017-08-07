226eda0828c4a10c82571ffc9dd9bcf4a8b615e9
Sean <swbrooks87@gmail.com>
2017-08-06 22:04:57 -0400

    Added Rails views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,18 +1,13 @@
 class TweetsController < ApplicationController
-  before_action :authorize_user, except: [:index, :show]
+  before_action :authenticate_user!
   before_action :check_user, only: [:edit, :update, :destroy]
-
-  def check_user
-    @tweet = Tweet.find(params[:id])
-    if @tweet.user != current_user || !current_user.admin?
-      redirect_to tweets_path, alert: "Sorry, you don't have persmissions."
-    end
-  end
 
   def index
     @tweets = Tweet.search(params[:search])
-    unless params[:search]
-      Tweet.order(updated_at: :desc).limit(20)
+    if params[:search]
+      Tweet.search(params[:search])
+    else
+      Tweet.all
     end
     @tweets.order(updated_at: :desc).limit(20)
   end
@@ -27,10 +22,48 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    @tweet.update_attributes(tweet_params)
+    if @tweet.save
+      redirect_to tweet_path(@tweet), notice: "Post has been updated successfully."
+    else
+      render action: 'edit'
+    end
+  end
+
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = current_user.id
-    binding.pry
+    @stock = StockQuote::Stock.json_quote(@tweet.ticker)
+    @pegratio = @stock["quote"]["PEGRatio"].to_f
+    @ask = @stock["quote"]["Ask"].to_f
+    @fiftyday = @stock["quote"]["FiftydayMovingAverage"].to_f
+    @buy = ((0.7) * @pegratio + (0.3) * (@ask - @fiftyday))
+    @position = "-"
+    if @buy < 1
+      @position = "Buy"
+    end
+
+    if @stock["quote"]["symbol"] == nil || @stock["quote"]["Ask"] == nil
+      binding.pry
+      render :new
+    else
+      @new_tweet = Tweet.create(
+        ticker: @stock["quote"]["symbol"],
+        ask: @stock["quote"]["Ask"],
+        percent_change: @stock["quote"]["PercentChange"],
+        market_capitalization: @stock["quote"]["MarketCapitalization"],
+        rating: @position,
+        body: @tweet.body,
+        user_id: current_user.id)
+
+      redirect_to tweets_path, notice: "Post was completed successfully."
+    end
   end
 
   def destroy
@@ -44,9 +77,10 @@ class TweetsController < ApplicationController
     params.require(:tweet).permit(:ticker, :body)
   end
 
-  def authorize_user
-    if !user_signed_in?
-      raise ActionController::RoutingError.new("Not Found")
+  def check_user
+    @tweet = Tweet.find(params[:id])
+    if @tweet.user != current_user
+      redirect_to tweets_path, alert: "Sorry, you don't have persmissions."
     end
   end
 end
