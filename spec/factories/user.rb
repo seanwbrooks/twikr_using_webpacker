97c3296c368be35e3e20615450135184f30e18d9
Sean <swbrooks87@gmail.com>
2017-08-18 10:14:06 -0400

    Added testing for tweets

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     first_name 'John'
     last_name 'Smith'
     username 'jsmith'
-    sequence(:email) { |n| "person#{n}@example.com" }
+    sequence(:email){ |n| "person#{n}@example.com" }
     password 'password'
     password_confirmation 'password'
   end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,15 +1,31 @@
 require 'rails_helper'
 
 describe Tweet do
+  #validate associations
   it { should belong_to :user }
 
+  #validations with expected input
   it { should have_valid(:ticker).when('tsla') }
-  it { should_not have_valid(:ticker).when(nil, '') }
-
   it { should have_valid(:body).when('This is a stock to watch out for.') }
-
   it { should have_valid(:ask).when('23.00') }
-
   it { should have_valid(:percent_change).when('-3.00%') }
 
+  #validations with invalid input
+  it { should_not have_valid(:ticker).when(nil, '') }
+end
+
+RSpec.describe 'Tweet' do
+  context 'making new tweet' do
+    let(:user){ FactoryGirl.build(:user) }
+    let(:tweet){ FactoryGirl.build(:tweet) }
+
+    it 'has a valid inputs' do
+      expect(tweet.ticker).to include("TSLA")
+      expect(tweet.ask).to include("329.87")
+      expect(tweet.percent_change).to include("3.45%")
+      expect(tweet.rating).to include("Buy")
+      expect(tweet.body).to include("This is body is used for testing")
+      expect(tweet.user_id).to be(1)
+    end
+  end
 end
