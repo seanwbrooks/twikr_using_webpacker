44347c7dc380111f5909f7f3f4208b878e78946b
Sean <swbrooks87@gmail.com>
2017-08-18 14:05:25 -0400

    Added review testing

---
+++ spec/factories/review.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :review do
+    comment "This is a test for comment."
+    user_id 1
+    tweet_id 1
+  end
+end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,9 +1,33 @@
 require 'rails_helper'
 
 describe Review do
+  #validates associations
   it { should belong_to :tweet }
 
+  #validate expected inputs
   it { should have_valid(:comment).when('This is a comment.') }
-  it { should_not have_valid(:comment).when(nil, '') }
 
+  #validates invalid inputs
+  it { should_not have_valid(:comment).when(nil, '') }
+end
+
+RSpec.describe "Review" do
+  context "create a review" do
+    #initialize models
+    let(:user){ FactoryGirl.build(:user) }
+    let(:tweet){ FactoryGirl.build(:tweet) }
+    let(:review){ FactoryGirl.build(:review) }
+
+    it "has a comment" do
+      expect(review.comment).to include("This is a test for comment.")
+    end
+
+    it "has user association" do
+      expect(review.user_id).to be(1)
+    end
+
+    it "has tweet association" do
+      expect(review.tweet_id).to be(1)
+    end
+  end
 end
