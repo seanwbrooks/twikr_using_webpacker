9f8496428afab56db77fce3e9d26c1b9b92b7207
Sean <swbrooks87@gmail.com>
2018-01-19 14:39:50 -0500

    Update review controller to call review_mailer

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -8,8 +8,12 @@ class Api::V1::ReviewsController < ApplicationController
   end
 
   def create
-    data = JSON.parse(request.body.read)
-    @new_review = Review.create(tweet_id: data["tweet_id"], user_id: current_user.id, comment: data["comment"])
+    @data = JSON.parse(request.body.read)
+    @new_review = Review.new(tweet_id: @data["tweet_id"], user_id: current_user.id, comment: @data["comment"])
+
+    if @new_review.save
+      ReviewMailer.new_review(@new_review).deliver_later
+    end
     @tweets = Tweet.order(updated_at: :desc).limit(20)
     render json: @tweets, adapter: :json
   end
